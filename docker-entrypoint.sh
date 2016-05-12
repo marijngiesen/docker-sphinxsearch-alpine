d3d296fd399a193a97982965fc2380a61f2f872a
marijngiesen <marijn@studio-donder.nl>
2016-05-12 17:21:42 +0200

    Try to run the indexer before starting searchd if the configuration exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,11 @@ if [ "${1#-}" != "$1" ]; then
 	set -- searchd "$@"
 fi
 
+if [ "$1" = 'searchd' ]; then
+  # If the sphinx config exists, try to run the indexer before starting searchd
+  if [ -f /etc/sphinx/sphinx.conf ]; then
+    exec "indexer --all"
+  fi
+fi
+
 exec "$@"
